7f0bbf97235f5b30480b26cd225c1e42186a15fe
Ferdinand John Briones <fjbriones@outlook.com>
2019-06-06 23:08:53 +0800

    Add page for adding to items

--- api/routes.js
+++ api/routes.js
@@ -1,4 +1,9 @@
 module.exports = function(app) {
 	var crud_commands = require('./crud');
 	app.get('/', crud_commands.list_items);
+
+	//Render add.ejs
+	app.get('/add', function(req, res) {
+		res.render('pages/add')
+	})
 }
