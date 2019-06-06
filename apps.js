c06d72c1ba2ce4c1a7b3e81a284a98e31d6ae70f
Ferdinand John Briones <fjbriones@outlook.com>
2019-06-06 09:30:05 +0800

    Initialize mysql database and table

--- apps.js
+++ apps.js
@@ -1,10 +1,32 @@
+//Declare dependencies here
 var express = require('express');
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
 
+//Initialize express engine server
 var app = express();
-
-const port = 3000;
-
 app.set('view engine', 'ejs');
 
+//Initialize listening to an port
+const port = 3000;
 app.listen(port);
+
+//Initialize connection to mysql database
+var mysql_con = mysql.createConnection({
+	host: "localhost",
+	user: "user",
+	password: "user",
+	database: "inventory"
+})
+
+//Connect to database and create a table
+mysql_con.connect(function(err) {
+	if (err) throw err;
+	console.log("Looking for TABLE items")
+	var createItemsTable = "CREATE TABLE IF NOT EXISTS items(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name varchar(100) NOT NULL, qty INT NOT NULL, amount DECIMAL(10,2) NOT NULL)";
+	mysql_con.query(createItemsTable, function(err2, results, fields){
+		if(err2) {
+			console.log(err2);
+		}
+	})
+})
