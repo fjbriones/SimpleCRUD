b5a20f83ee5d45756481153c5ce5c190dd89921d
Ferdinand John Briones <fjbriones@outlook.com>
2019-06-06 22:28:31 +0800

    Completed initial api for crud

--- api/crud.js
+++ api/crud.js
@@ -5,3 +5,27 @@ exports.list_items = function(req, res) {
 	})
 	res.render('pages/home')
 }
+
+exports.add_item = function(req, res) {
+	var sql_com_add = "INSERT INTO items (name, qty, amount) VALUES (?, ?, ?)";
+	db.query(sql_com_add, [req.body.name, req.body.qty, req.body.amount], function(err, result, fields) {
+		if(err) throw err;
+		console.log("1 record inserted into items");
+	})
+}
+
+exports.update_item = function(req, res) {
+	var sql_com_update = "UPDATE items SET name=?, qty=?, amount=? WHERE id=?";
+	db.query(sql_com_update, [req.body.name, req.body.qty, req.body.amount, req.body.id], function(err, result, fields) {
+		if(err) throw err;
+		console.log("1 record updated in items");
+	})
+}
+
+exports.delete_item = function(req, res) {
+	var sql_com_delete = "DELETE FROM items WHERE id=?";
+	db.query(sql_com_delete, [req.body.id], function(err, result, fields) {
+		if(err) throw err;
+		console.log("1 record deleted in items");
+	})
+}
